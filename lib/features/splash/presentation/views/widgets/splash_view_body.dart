0bcbf29181d0f5f419d3e302ec439bbad25f9690
khalidadel365 <95598763+khalidadel365@users.noreply.github.com>
2025-06-23 02:49:35 +0300

    adding slidinganimation to splash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,45 @@
 import 'package:bookly_app/core/utilities/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin{
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+        vsync: this,
+      duration: const Duration(
+        seconds: 1
+      )
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0 , 2),
+      end: Offset.zero,
+    ).animate(animationController );
+    animationController.forward();
+    slidingAnimation.addListener((){
+      setState(() {
+
+      });
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,15 +50,10 @@ class SplashViewBody extends StatelessWidget {
           AssetsData.logo,
           ),
         SizedBox(height: 7),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
 }
+
+
