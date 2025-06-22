78a9546db800a818ff84656e85ed5612fe49b1de
khalidadel365 <95598763+khalidadel365@users.noreply.github.com>
2025-06-23 01:45:19 +0300

    first commit

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/core/utilities/assets.dart';
+import 'package:flutter/cupertino.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          AssetsData.logo,
+          ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+    );
+  }
+}
+
