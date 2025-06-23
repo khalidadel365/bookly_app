9879ca54582ee52933a94985bc84c64928f08f9b
khalidadel365 <95598763+khalidadel365@users.noreply.github.com>
2025-06-24 00:33:05 +0300

    add featured list view to home

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -2,24 +2,21 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/utilities/assets.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*.28,
-      child: AspectRatio(
-        aspectRatio: 2.7/4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16  ),
-              color: Colors.blue,
-              image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.testImage)
-              )
-          ),
+    return AspectRatio(
+      aspectRatio: 2.7/4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16  ),
+            color: Colors.blue,
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AssetsData.testImage)
+            )
         ),
       ),
     );
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import 'custom_app_barr.dart';
+import 'custom_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -15,20 +15,30 @@ class HomeViewBody extends StatelessWidget {
       children: const [
         CustomAppBar(),
         SizedBox(height: 10),
-        CustomListView(),
+        FeaturedBooksListView(),
       ],
     );
   }
 }
-class CustomListView extends StatelessWidget {
-  const CustomListView({super.key});
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (context,index)=> CustomListViewItem(),
-        separatorBuilder: (context,index)=>const SizedBox(width: 10),
-        itemCount: 5);
+    return SizedBox(
+      height: MediaQuery.of(context).size.height*.28,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context,index)=> FeaturedListViewItem(),
+            itemCount: 10,
+            separatorBuilder: (context,index)=>SizedBox(
+              width: 12),
+        ),
+      ),
+    );
   }
 }
 
