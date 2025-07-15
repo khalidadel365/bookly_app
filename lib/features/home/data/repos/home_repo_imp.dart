fe34caf788337005ebba57a0994839fe45d8f72e
khalidadel365 <95598763+khalidadel365@users.noreply.github.com>
2025-07-16 00:42:40 +0300

    implement fetch used books

--- lib/features/home/data/repos/home_repo_imp.dart
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -1,12 +1,26 @@
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utilities/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImp implements HomeRepo{
+  final ApiService apiService;
+  HomeRepoImp(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async{
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming'
+      );
+      List<BookModel> books = [];
+      for(var item in data['items']){
+        books.add(BookModel.fromMap(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
