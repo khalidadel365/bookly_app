2320a2039f38c3ac25177d36056bf4ce2180f4a7
khalidadel365 <95598763+khalidadel365@users.noreply.github.com>
2025-07-25 01:44:10 +0300

    add get_it and setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utilities/app_router.dart';
+ import 'package:bookly_app/core/utilities/app_router.dart';
+import 'package:bookly_app/core/utilities/sevice_locator.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_imp.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'features/splash/presentation/views/splash_view.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
@@ -22,14 +28,22 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme.apply(bodyColor: Colors.white)
-        )
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=>FeaturedBooksCubit(
+            getIt.get<HomeRepoImp>())),
+        BlocProvider(create: (context)=>NewestBooksCubit(
+            getIt.get<HomeRepoImp>()),),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme.apply(bodyColor: Colors.white)
+          )
+        ),
       ),
     );
   }
